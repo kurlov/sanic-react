bf1738aeca4b0011cc484c8f989e0618b1e60437
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2017-12-10 21:07:42 +0500

    Added pip and sanic installation

--- Vagrantstartup.sh
+++ Vagrantstartup.sh
@@ -1,7 +1,10 @@
 apt-get -qqy update
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-apt-get -qqy install -y nodejs
+sudo apt-get install -y nodejs
 sudo npm install -g webpack
 cd /vagrant
 npm install --no-bin-links
 npm run build
+curl https://bootstrap.pypa.io/get-pip.py | sudo python3
+sudo apt-get -qqy install build-essential libssl-dev libffi-dev python3-dev
+sudo pip install sanic
