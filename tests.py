265862fe91d68b3a405be85d86ff246d13730974
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2017-12-17 23:42:36 +0500

    added basic tests for sanic app

---
+++ tests.py
@@ -0,0 +1,10 @@
+from main import app
+
+
+def test_fixture_test_client_get():
+    req, resp = app.test_client.get('/')
+    assert resp.status == 200
+
+def test_index_put_not_allowed():
+    req, resp = app.test_client.put('/')
+    assert resp.status == 405
