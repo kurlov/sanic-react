f970db2d4cd6d83ff6ef19522e6833ae922c099e
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2017-12-20 22:24:09 +0500

    Added different sanic routes

--- main.py
+++ main.py
@@ -9,5 +9,19 @@ app.static('/', './dist')
 async def index(request):
     return await file('dist/index.html')
 
+@app.route("/example_post", methods=["POST",])
+def create_user(request):
+    return text("POST data: %s" % request.body)
+
+@app.route("/example_json")
+def post_json(request):
+    return json({ "received": True, "data": request.json })
+
+@app.route("/query_string")
+def query_string(request):
+    return json({ "parsed": True, "args": request.args, "url": request.url,
+                  "query_string": request.query_string })
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
